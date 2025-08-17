387e40db3f699290c46c5d0d8419ab574ee19465
Adnan Fito <adnanfito26@gmail.com>
2025-08-17 11:18:17 +0700

    fix: image

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,27 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Expose folder "uploads" di URL /uploads
+  const uploadsDir = join(process.cwd(), 'uploads');
+  app.use('/uploads', express.static(uploadsDir));
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`🚀 API: http://0.0.0.0:${process.env.PORT ?? 3000}`);
+  console.log(`📦 Static: /uploads -> ${uploadsDir}`);
 }
 bootstrap();
