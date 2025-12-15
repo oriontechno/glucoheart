9485985b8e89111f8ccd4201d106dfef2082d752
dzakwanIrfan <lexdani368@gmail.com>
2025-12-15 22:26:07 +0700

    feat: initialize NestJS application with static file serving and CORS configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,18 +6,19 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Expose folder "uploads" agar gambar bisa diakses
   const uploadsDir = join(process.cwd(), 'uploads');
   app.use('/uploads', express.static(uploadsDir));
 
-  // Konfigurasi CORS yang Benar
   app.enableCors({
-    origin: '*',
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://glucoheart.vercel.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
-  // Pastikan port sesuai
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
